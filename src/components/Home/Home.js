db61da65887be57a05049724a634d8e2f597b276
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-11-08 18:22:46 +0600

    react-photo-view added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import banner from '../images/banner.jpg'
 import './Home.css'
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const Home = () => {
     const services = useLoaderData()
@@ -19,7 +21,12 @@ const Home = () => {
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 my-8 mx-12'>
                 {
                     services.map(service => <div className="card  bg-base-100 shadow-xl " key={service._id}>
-                        <figure><img className='service-img' src={service.image_url} alt="Movie" /></figure>
+                        <figure>  <PhotoProvider>
+                            <PhotoView src={service.image_url}>
+
+                                <img className='service-img' src={service.image_url} alt="Movie" />
+                            </PhotoView>
+                        </PhotoProvider></figure>
                         <div className="card-body">
                             <h2 className="card-title"></h2>
                             <p className='text-3xl font-bold'>{service.title}</p>
